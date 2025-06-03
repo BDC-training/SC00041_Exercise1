412d1314ef169c09e4f42a3637d03cd7a780c2d0
AndreIdegard <137905805+AndreIdegard@users.noreply.github.com>
2025-06-03 09:29:41 +0200

    Added code printing the text file and added comments to the text.

--- analyze_text.py
+++ analyze_text.py
@@ -1,19 +1,23 @@
-My_file = "/home/user/Desktop/sample.txt" 
+My_file = "/Users/xidega/SC00041_Exercise1-group4/sample.txt" # Defining file path  
 
-print("Line\tChars\tUppercase\t% Upper")
+f = open(My_file) # open file at path
 
-f = open(My_file)
-i = 1
+print(f.read()) # print the contents of the file to get an overview
+
+print("Line\tChars\tUppercase\t% Upper") # print column names for table
+
+f = open(My_file) # open file again
+i = 1 # define iteration variable i
 for l in f:
-    total = len(l.strip())
+    total = len(l.strip()) #Number of characters of row
     upper = 0
     for c in l:
-        if c.isupper():
+        if c.isupper(): # Number of uppercase letters in row
             upper += 1
     if total != 0:
-        p = upper / total * 100
+        p = upper / total * 100 # Calculating what proportion of all characters are uppercase then multiolyign by 100 to get as percent.
     else:
         p = 0
-    print(str(i) + "\t" + str(total) + "\t" + str(upper) + "\t" + str(round(p, 2)) + "%")
+    print(str(i) + "\t" + str(total) + "\t" + str(upper) + "\t" + str(round(p, 2)) + "%") #print data calculated above.
     i += 1
 
